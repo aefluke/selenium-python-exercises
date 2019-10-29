20514c763472b58902118c2b5ab6fb49a64dd25a
KSNET\09370478 <ahmet6yilmaz@gmail.com>
2019-10-29 21:46:03 +0300

    assertion fixed

--- 1_using_pytest_3_1_test.py
+++ 1_using_pytest_3_1_test.py
@@ -18,4 +18,4 @@ def test_3_1():
                  str(current_time.tm_min) + ":" +
                  str(current_time.tm_sec)
                  )
-    assert current_time.tm_hour < 9, "Current time should be before noon"
+    assert current_time.tm_hour < 12, "Current time should be before noon"
